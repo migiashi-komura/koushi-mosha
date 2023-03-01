4ce1ded568307409a4dbb49e13ac2c264e9f46ed
migiashi <migiashi.komura@gmail.com>
2023-03-01 22:54:52 +0900

    ほんの少し修正

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import cv2
 import numpy as np
 
 # read the image
-image = cv2.imread("./image.jpg")
+image_path = input("enter the image path: ")
+image = cv2.imread(image_path)
 
 height = image.shape[0]
 width = image.shape[1]
@@ -14,21 +15,26 @@ paper.fill(255)
 # how may lines 
 lines = int(input("how may lines for each: "))
 
+# line thickness
+thickness = 2
+
 for i in range(lines):
     # only integer is accepted
     pt1x = int(width / (lines + 1)) * (i + 1)
     pt1y = 0
     pt2x = pt1x
     pt2y = height
-    cv2.line(image, (pt1x, pt1y), (pt2x, pt2y), (0, 0, 0), 4)
-    cv2.line(paper, (pt1x, pt1y), (pt2x, pt2y), (0, 0, 0), 4)
+    cv2.line(image, (pt1x, pt1y), (pt2x, pt2y), (0, 0, 0), thickness)
+    cv2.line(paper, (pt1x, pt1y), (pt2x, pt2y), (0, 0, 0), thickness)
 
     pt1x = 0
     pt1y = int(height/ (lines + 1)) * (i + 1)
     pt2x = width
     pt2y = pt1y
-    cv2.line(image, (pt1x, pt1y), (pt2x, pt2y), (0, 0, 0), 4)
-    cv2.line(paper, (pt1x, pt1y), (pt2x, pt2y), (0, 0, 0), 4)
+    cv2.line(image, (pt1x, pt1y), (pt2x, pt2y), (0, 0, 0), thickness)
+    cv2.line(paper, (pt1x, pt1y), (pt2x, pt2y), (0, 0, 0), thickness)
 
-
-cv2.imwrite("./export.jpg", cv2.hconcat([paper, image]))
+# make a new image
+export_img = cv2.hconcat([paper, image])
+cv2.line(export_img, (width, 0), (width, height), (0, 0, 0), thickness)
+cv2.imwrite("./export.jpg", export_img)
